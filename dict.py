9ec58d802907ef67318d513f82bfcb179670143a
AbdelmalekYounes <abdelmalekyounes31@gmail.com>
2024-04-07 15:54:03 +0200

    API Implementation + Languages

---
+++ dict.py
@@ -0,0 +1,39 @@
+
+# -*- coding: utf-8 -*-
+
+def trad() :
+    traductions = {
+        'fr': {
+            'morning': "Bonjour",
+            'afternoon': "Bon apres-midi",
+            'evening': "Bonne soiree",
+            'well_said': "Bien dit",
+        },
+        'en': {
+            'morning': "Good morning",
+            'afternoon': "Good afternoon",
+            'evening': "Good evening",
+            'well_said': "well said",
+        },
+        'es':{
+            'morning': "Hola",
+            'afternoon':'Buenas tardes',
+            'evening':'Buenas noches', 
+            'well_said':"¡Muy bien dicho!"
+        },
+        'de':{
+            'morning':'Guten Morgen!',
+            'afternoon':'Guten Tag!',
+            'evening':'Abend allein',
+            'well_said':'Sehr gut gesagt!'
+        },
+        'it':{
+            'morning':'Buongiorno',
+            'afternoon':'Buona pomer',
+            'evening':'Buona sera',
+            'well_said':'Ben detto'
+        }
+        
+    }
+    
+    return traductions
